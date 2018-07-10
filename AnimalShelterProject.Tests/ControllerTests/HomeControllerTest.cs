4f4b69374ab86e95979ac6635cc915dc8e4feb7f
Abel Trotter and Ryan Murry <abel@>
2018-07-10 08:28:52 -0700

    set up database

---
+++ AnimalShelterProject.Tests/ControllerTests/HomeControllerTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using AnimalShelterProject.Controllers;
+using AnimalShelterProject.Models;
+
+namespace AnimalShelterProject.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+
+    }
+}
---
+++ AnimalShelterProject.Tests/ModelTests/Animal.Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using AnimalShelterProject.Models;
+
+namespace AnimalShelterProject.Tests
+{
+    [TestClass]
+    public class AnimalTest
+    {
+
+    }
+}
